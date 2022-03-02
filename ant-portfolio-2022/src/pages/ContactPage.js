77a178fbf70df34779f38847f2f4a39e99cc47f6
anthony-gordon <anthonylebrungordon@gmail.com>
2022-03-02 18:45:03 +1300

    spinner

--- ant-portfolio-2022/src/pages/ContactPage.js
+++ ant-portfolio-2022/src/pages/ContactPage.js
@@ -3,9 +3,14 @@ import React, { useEffect, useContext } from "react";
 import { ShopContext } from "../context/shopContext";
 import { Helmet } from "react-helmet-async";
 import { useParams } from "react-router-dom";
+import Loading from "./../sub-components/Loading";
 
 function ContactPage() {
-  return <div className="ContactPage"></div>;
+  return (
+    <div className="ContactPage">
+      <Loading />
+    </div>
+  );
 }
 
 export default ContactPage;
