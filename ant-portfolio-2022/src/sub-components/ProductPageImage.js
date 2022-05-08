e81e1598d07a8c1496eedf388e9af2cb79b6173f
anthony-gordon <anthonylebrungordon@gmail.com>
2022-05-08 16:16:37 +1200

    product page image working

--- ant-portfolio-2022/src/sub-components/ProductPageImage.js
+++ ant-portfolio-2022/src/sub-components/ProductPageImage.js
@@ -1,7 +1,64 @@
+import React, { useState, useEffect, useRef } from "react";
 import "../style/sub-components/ProductPageImage.css";
 
+import { useSpring, animated as a } from "react-spring";
+import { useSelector } from "react-redux";
+
 function ProductPageImage({ src, size, aspect, product }) {
-  return <img src={src} className="ProductPageImage" />;
+  const figure = useRef(null);
+  const hoverDevice = useSelector((state) => state.hoverDevice);
+
+  const [{ mousePositionX, mousePositionY }, setMousePosition] = useSpring(
+    () => ({
+      mousePositionX: [0],
+      mousePositionY: [0],
+
+      config: {
+        mass: 1,
+        tension: 200,
+        friction: 30,
+        precision: 0.00001,
+        velocity: 0,
+        clamp: true,
+      },
+    })
+  );
+  useEffect(() => {
+    if (hoverDevice) {
+      const handleMouseMove = (e) => {
+        const { left, top, width, height } = e.target.getBoundingClientRect();
+        const x = ((e.clientX - left) / width) * 100;
+        const y = ((e.clientY - top) / height) * 100;
+        setMousePosition({
+          mousePositionX: [x],
+          mousePositionY: [y],
+        });
+      };
+      figure.current.addEventListener("mousemove", (e) => handleMouseMove(e));
+      return () =>
+        figure.current.removeEventListener("mousemove", (e) =>
+          handleMouseMove(e)
+        );
+    }
+  });
+
+  return (
+    <a.figure
+      style={{
+        backgroundPositionX: mousePositionX.to(
+          (mousePositionX) => `${mousePositionX}%, 0%`
+        ),
+        backgroundPositionY: mousePositionY.to(
+          (mousePositionY) => `${mousePositionY}%, 0%`
+        ),
+        backgroundImage: `url(${src})`,
+      }}
+      ref={figure}
+      className="ProductPageImage"
+    >
+      <img src={src} className="ProductPageImage__image" />
+    </a.figure>
+  );
 }
 
 export default ProductPageImage;
